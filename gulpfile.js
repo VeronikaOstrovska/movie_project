b867424d7ad1647eefec7c103b89dd6589cf832a
Nika Ostrovska <nika.ostrovska21@gmail.com>
2019-06-20 19:40:47 +0300

    Loading plugins and adding new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-const gulp = require('gulp');
-const webserver = require('gulp-webserver');
+const gulp = require('gulp'),
+      webserver = require('gulp-webserver'),
+      less = require('gulp-less'),
+      imagemin = require('gulp-imagemin'); // Сжатие изображений
+      livereload = require('gulp-livereload');
+      connect = require('gulp-connect');
+      watch = require('gulp-watch');
 
-
+//Запуск вебсервера
 gulp.task('webserver', function() {
-  gulp.src('html/')
+  gulp.src('build/')
     .pipe(webserver({
       livereload: false,
       port:8002,
@@ -11,13 +16,45 @@ gulp.task('webserver', function() {
     }));
 });
 
+// Копирование HTML-файлов в папку build
+gulp.task("html", function() { 
+    return gulp.src("html/*.html")
+    .pipe(gulp.dest("build"));
+    .pipe(connect.reload());
+});
+
+//Компиляция Less в Css
+gulp.task('less', function () {
+    return gulp.src('html/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('build/css'));
+        .pipe(connect.reload());
+});
+
+// Сжимаем картинки
+gulp.task('imgs', function() {
+    return gulp.src("html/images/*.+(jpg|jpeg|png|gif)")
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true
+        }))
+        .pipe(gulp.dest("build/images"));
+        .pipe(connect.reload());
+});
 
 
+// Задача слежения за измененными файлами
+gulp.task("watch", function() {
+    gulp.watch("html/*.html", ["html"]);
+    gulp.watch("html/less/*.less", ["less"]);
+    gulp.watch("html/images/*.+(jpg|jpeg|png|gif)", ["imgs"]);
+});
 
 gulp.task(
   'default',
   gulp.series([
   
-    'webserver'
+    'html', 'less', 'imgs', 'webserver', 'watch'
   ])
 );
